a7a43cd7f18d4e6414304afc76f7fae69024b10a
k74339562-cloud <k74339562@gmail.com>
2026-09-16 19:39:51 +0300

    Implement gridmap2 module initialization and termination

---
+++ src/register_types.cpp
@@ -0,0 +1,30 @@
+#include "register_types.h"
+#include "grid_map_2.h"
+#include <gdextension_interface.h>
+#include <godot_cpp/core/defs.hpp>
+#include <godot_cpp/godot.hpp>
+
+using namespace godot;
+
+void initialize_gridmap2_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+    ClassDB::register_class<GridMap2>();
+}
+
+void uninitialize_gridmap2_module(ModuleInitializationLevel p_level) {
+    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+        return;
+    }
+}
+
+extern "C" {
+GDExtensionBool GDE_EXPORT gridmap2_init(GDExtensionInterfaceGetProcAddress p_get_proc_address, const GDExtensionClassLibraryPtr p_library, GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_get_proc_address, p_library, r_initialization);
+    init_obj.register_initializer(initialize_gridmap2_module);
+    init_obj.register_terminator(uninitialize_gridmap2_module);
+    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+    return init_obj.init();
+}
+}
